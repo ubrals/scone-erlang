83325ea3ab8c334eddcbac30058a8b6482aa6b3f
Andre Miguel <andre_augusto.miguel@mailbox.tu-dresden.de>
2021-04-21 15:08:55 +0000

    Image update: BPFTRACE shell script with SCONE_* env. vars

--- progs/exec_bpftrace_strace.sh
+++ progs/exec_bpftrace_strace.sh
@@ -1,2 +1,2 @@
-sudo /mnt/bpft/bpftrace_static --include /mnt/bpft/missing_types_mig.h union_cleaner_inner_cgroup.bt -o ./_bpft.union.`date '+%Y%m%d%H%M%S'`.log -c '/bin/sh -c ./exec_erlang_scone_strace.sh'
+sudo -E BPFTRACE_MAX_MAP_KEYS=5120000 -E SCONE_BPFTRACE=1 -E SCONE_MAP_LAYOUT=1 -E SCONE_MODE=sim /mnt/bpft/bpftrace_static --include /mnt/bpft/missing_types_mig.h union_cleaner_inner_cgroup_profiler.bt -o ./_bpft.union.`date '+%Y%m%d%H%M%S'`.log -c '/bin/sh -c ./exec_erlang_scone_strace.sh'
 
